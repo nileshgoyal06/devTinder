409bc61911cda0c01256bdda2768fe946c313d9e
Nilesh <nileshgoyal624@gmail.com>
2025-03-01 16:06:14 +0530

    explore postman

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const app =express();
 
 app.use("/nilesh",(req,res)=>{
     res.send("Hello from Nilesh");
-})
+}) 
 
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName:"Nilesh",
+        lastName:"Goyal"
+    });
+})
 app.use("/",(req,res)=>{
     res.send("Hello World");
 })
 
 app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+    console.log("Server is running on port 5000"); 
 })
