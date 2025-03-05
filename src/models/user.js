18da27fa93e1efa97804a97cabf68a12140cbe39
Nilesh <nileshgoyal624@gmail.com>
2025-03-05 15:54:46 +0530

    use validator library for password and email

--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,58 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
+const validator = require("validator");  // library to use add validation in email,password for strongness and in email(@,.com,gmail) includes validation 
+
+const userSchema = mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        minlength:4,
-        maxlength: 20
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 20,
     },
-    lastName:{
-        type:String
+    lastName: {
+      type: String,
     },
-    emailid:{
-        type:String,
-        required:true,
-        unique:true
+    emailid: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email: " + value);
+        }
+      },
     },
-    age:{
-        type:Number
+    age: {
+      type: Number,
     },
-    password:{
-        type:String
+    password: {
+      type: String,
+      required: true,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("password is not strong enough");
+        }
+      },
     },
-    gender:{
-        type:String,
-        validate(value){
-            if(!["male","female","others"].includes(value)){
-                throw new Error("Invalid gender");
-            }
-        },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value.toLowerCase())) {
+          throw new Error("Invalid gender");
+        }
+      },
     },
-    photoUrl:{
-        type:String,
-        default:"https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png"
+    photoUrl: {
+      type: String,
+      default: "https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png",
     },
-    skills:{
-        type:[String], 
-    }
-
-
-},
-{
-    timestamps:true
-}
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true, 
+  }
 );
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
