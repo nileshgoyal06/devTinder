90c469839ddbcf09c8b49de5160af607f6ceccdf
Nilesh <nileshgoyal624@gmail.com>
2025-06-28 11:25:55 +0530

    Payment integration

--- src/models/user.js
+++ src/models/user.js
@@ -1,65 +1,89 @@
 const mongoose = require("mongoose");
-const validator = require("validator");  // library to use add validation in email,password for strongness and in email(@,.com,gmail) includes validation 
+const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const userSchema = mongoose.Schema(
+
+const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
       required: true,
-      minlength: 4,
-      maxlength: 20,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
       type: String,
     },
     emailid: {
       type: String,
+      lowercase: true,
       required: true,
       unique: true,
+      trim: true,
       validate(value) {
         if (!validator.isEmail(value)) {
-          throw new Error("Invalid Email: " + value);
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
         }
       },
     },
     age: {
       type: Number,
-    },
-    password: {
-      type: String,
-      required: true,
-      validate(value){
-        if(!validator.isStrongPassword(value)){
-            throw new Error("password is not strong enough");
-        }
-      },
+      min: 18,
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value.toLowerCase())) {
-          throw new Error("Invalid gender");
-        }
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
       },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
+    },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
     },
     photoUrl: {
       type: String,
-      default: "https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png",
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
     },
     skills: {
       type: [String],
     },
   },
   {
-    timestamps: true, 
+    timestamps: true,
   }
 );
 
 userSchema.methods.getJWT = async function () {
   const user = this;
 
-  const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
     expiresIn: "7d",
   });
 
@@ -70,7 +94,10 @@ userSchema.methods.validatePassword = async function (passwordInputByUser) {
   const user = this;
   const passwordHash = user.password;
 
-  const isPasswordValid = await bcrypt.compare( passwordInputByUser, passwordHash);
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
 
   return isPasswordValid;
 };
